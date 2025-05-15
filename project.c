6cc89fd2c060ac892aee97417fdde92c0b8bd260
Kim Jinseong <kjih1999@naver.com>
2025-05-15 21:32:42 +0900

    Initial commit

---
+++ project.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void){ // 매개변수 사용하지 않겠다. {
+
+    int a =0, b=0; 
+    scanf("%d,%d", &a,&b); 
+    
+    if (a > 0){
+        if (b == 0){
+            a = 0; 
+            printf("%d,%d", a,b); 
+        } else {
+            printf("%d,%d", a,b); 
+        }
+    } else {
+        a = -1 ;
+        printf("%d,%d", a,b);
+    }
+
+}
