841cf4d8e915c4d7239531721af4f335829e2c35
Kim Jinseong <kjsng20@gmail.com>
2025-07-30 15:11:53 +0900

    .

--- test.c
+++ test.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <string.h>
-
-int main(void){ 
-
-    char a[] = "banana"; 
-    char b[] = "apple"; 
-
-    strcmp(a,b); 
-    
-    int length = strlen(a); 
-
-    printf("%s", a);
-
+#include<stdio.h>
+int C(int v){
+    printf("%d", v); 
+    return 1; 
+}
+int main(){
+    int a = -2;
+    int b = !a; 
+    printf("%d %d %d %d ", a, b, a&&b, a||b); 
+    if(b && C(10))
+        printf("A "); 
+    if(b & C(20))
+        printf("B "); 
+    return 0; 
 }
